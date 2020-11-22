c4e01f7a97dbe13f7f1fff5ad217d3365818281a
Jacob Ebey <jacob.ebey@live.com>
2020-11-22 02:51:38 -0800

    Updated vercel config

--- ssr_react_streaming_example_b/api/ssr.js
+++ ssr_react_streaming_example_b/api/ssr.js
@@ -3,7 +3,9 @@ import bootstrap from "../dist/server/runtime/main";
 const initPromise = bootstrap().then((r) => r.default);
 
 export default async (req, res) => {
-  const server = await initPromise;
-  const html = await server("/b");
-  res.send(html);
+  try {
+    const server = await initPromise;
+    const html = await server("/b");
+    res.send(html);
+  }
 };
