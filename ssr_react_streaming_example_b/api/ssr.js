e61421e99ed2f3abc7bc9dc544ca494825eedbdf
Jacob Ebey <jacob.ebey@live.com>
2020-11-22 01:51:21 -0800

    Attempting Rewrites

--- ssr_react_streaming_example_b/api/ssr.js
+++ ssr_react_streaming_example_b/api/ssr.js
@@ -4,6 +4,6 @@ const initPromise = bootstrap().then((r) => r.default);
 
 export default async (req, res) => {
   const server = await initPromise;
-  const html = await server(req.path);
+  const html = await server("/b");
   res.send(html);
 };
