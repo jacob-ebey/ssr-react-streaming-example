3c310d728908b2dbd6dfe82a889ac93ddaa47ad5
Jacob Ebey <jacob.ebey@live.com>
2020-11-22 03:49:45 -0800

    Updated ssr to route on location query param

--- ssr_react_streaming_example_a/api/ssr.js
+++ ssr_react_streaming_example_a/api/ssr.js
@@ -5,7 +5,7 @@ const initPromise = bootstrap().then((r) => r.default);
 export default async (req, res) => {
   try {
     const server = await initPromise;
-    const html = await server("/b");
+    const html = await server(req.query.location || "");
     res.send(html);
   } catch (err) {
     console.error(err);
